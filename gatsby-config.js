2efdff8b71f605a0b3ee8435a85697a6c9321896
Fran Canete <francanetedev@gmail.com>
2021-08-03 08:31:49 +0100

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -11,9 +11,9 @@ module.exports = {
       twitter: `kylemathews`,
     },
   },
+  plugins: [`gatsby-plugin-netlify-cms`],
   plugins: [
     `gatsby-plugin-image`,
-    `gatsby-plugin-netlify-cms`,
     {
       resolve: `gatsby-source-filesystem`,
       options: {
