a6d226a05d311a60b956d8441d5646fb6c661502
Fran Canete <francanetedev@gmail.com>
2021-12-31 14:46:57 +0000

    Change icon

--- gatsby-config.js
+++ gatsby-config.js
@@ -120,7 +120,7 @@ module.exports = {
         background_color: `#ffffff`,
         theme_color: `#ffffff`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/images/logo.png`, // This path is relative to the root of the site.
       },
     },
     `gatsby-plugin-react-helmet`,
