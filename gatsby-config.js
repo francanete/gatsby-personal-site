3abf60e6dd3283eef40d8289721612d84e650180
Fran Canete <francanetedev@gmail.com>
2021-12-31 14:35:55 +0000

    Change theme colour

--- gatsby-config.js
+++ gatsby-config.js
@@ -118,7 +118,7 @@ module.exports = {
         short_name: `GatsbyJS`,
         start_url: `/`,
         background_color: `#ffffff`,
-        theme_color: `#663399`,
+        theme_color: `#ffffff`,
         display: `minimal-ui`,
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
