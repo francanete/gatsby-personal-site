56d810b535cadbc5100df35c1aaec81823474369
Fran Canete <francanetedev@gmail.com>
2022-01-26 21:37:18 +0000

    Upgrade react icons

--- src/components/MobileNavigation.js
+++ src/components/MobileNavigation.js
@@ -1,7 +1,7 @@
 import React from "react"
 import { NavLinks } from "./NavLinks"
-// import { CgMenuGridR } from "react-icons/Cg"
-// import { CgClose } from "react-icons/Cg"
+import { CgMenuGridR } from "react-icons/Cg"
+import { CgClose } from "react-icons/Cg"
 import { useState } from "react"
 
 import * as style from "../styles/NavBar.module.css"
@@ -9,28 +9,28 @@ import * as style from "../styles/NavBar.module.css"
 export const MobileNavigation = () => {
   const [open, setOpen] = useState(false)
 
-  // const hamburgerIcon = (
-  //   <CgMenuGridR
-  //     className={style.Hamburger}
-  //     size="30px"
-  //     color="black"
-  //     onClick={() => setOpen(!open)}
-  //   />
-  // )
+  const hamburgerIcon = (
+    <CgMenuGridR
+      className={style.Hamburger}
+      size="30px"
+      color="black"
+      onClick={() => setOpen(!open)}
+    />
+  )
 
-  // const closeIcon = (
-  //   <CgClose
-  //     className={style.Hamburger}
-  //     size="30px"
-  //     color="black"
-  //     onClick={() => setOpen(!open)}
-  //   />
-  // )
+  const closeIcon = (
+    <CgClose
+      className={style.Hamburger}
+      size="30px"
+      color="black"
+      onClick={() => setOpen(!open)}
+    />
+  )
 
   return (
     <nav className={style.MobileNavigation}>
-      {/* {open ? closeIcon : hamburgerIcon} */}
-      {open ? "O" : "X"}
+      {open ? closeIcon : hamburgerIcon}
+      {/* {open ? "O" : "X"} */}
       <div className={style.linksMobile}></div>
       {open && <NavLinks />}
     </nav>
